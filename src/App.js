aa931692e3f16310076f236ae39a2cc6aba74454
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-30 10:06:50 -0700

    Initial setup for react and firebase

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello sCart</h1>
+      {/* Header */}
+      {/* Home */}
+
+    </div>
+  );
+}
+
+export default App;
