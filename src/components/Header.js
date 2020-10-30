5e0b9f12f2be1069fbac0ae716c52bf3f9d26f40
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-30 14:20:58 -0700

    Add Shopping cart & count

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/Header.css';
-
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 function Header() {
   return (
@@ -8,21 +9,26 @@ function Header() {
 
       <img className='header__logo' src='https://bookable-rooms-images.s3.us-east-2.amazonaws.com/sCart-logo-black.png'/>
       <div className='header__search'>
-        <input className='header__searchInput' type='text' placeholder='Search for anything' />
+        <input className='header__searchInput' type='text' placeholder='Search..' />
+        <SearchIcon className='header__searchIcon'/>
 
       </div>
 
       <div className='header__nav'>
         <div className='header__option'>
           <span className='header__optionLineOne'>Hello Guest</span>
-        </div>
-
-        <div className='header__option'>
           <span className='header__optionLineTwo'>Sign In</span>
         </div>
 
+
         <div className='header__option'>
-           <span className='header__optionLineThree'>Cart</span>
+           <span className='header__optionLineOne'>Returns</span>
+          <span className='header__optionLineTwo'>& Orders</span>
+        </div>
+
+        <div className='headere__optionCart'>
+          <ShoppingCartIcon />
+          <span className='header__optionLineTwo header__basketCount'>0</span>
         </div>
 
       </div>
