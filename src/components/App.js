1c7f29beea0f670b7b47d8268226baab1004b24b
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-30 14:26:27 -0700

    Create Home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../styles/App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
       {/* <h1>Hello sCart</h1> */}
       <Header />
-      {/* Home */}
+      <Home />
 
     </div>
   );
