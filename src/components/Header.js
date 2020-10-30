845fde821f9848ae034e1ed42d7865b16dd65224
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-30 11:56:41 -0700

    Add sign-in and cart basic layout in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,25 @@ function Header() {
   return (
     <div className='header'>
 
-      <img className='header__logo' src='https://bookable-rooms-images.s3.us-east-2.amazonaws.com/sCart-logo-white.png'/>
+      <img className='header__logo' src='https://bookable-rooms-images.s3.us-east-2.amazonaws.com/sCart-logo-black.png'/>
       <div className='header__search'>
-        <input className='header__searchInput' type='text'>
+        <input className='header__searchInput' type='text' placeholder='Search for anything'>
         </input>
       </div>
 
       <div className='header__nav'>
         <div className='header__option'>
-          <span className='header__option'></span>
+          <span className='header__optionLineOne'>Hello Guest</span>
         </div>
+
+        <div className='header__option'>
+          <span className='header__optionLineTwo'>Sign In</span>
+        </div>
+
+        <div className='header__option'>
+           <span className='header__optionLineThree'>Cart</span>
+        </div>
+
       </div>
 
     </div>
