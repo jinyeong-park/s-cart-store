b6834d9e805c03eafa710620d9518cb535d1a914
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-30 15:04:34 -0700

    Add Product component in Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Home.css';
+import Product from './Product';
 
 function Home() {
   return (
     <div className='home'>
+      <div className='home_container'>
+        <img className='home_image' src='' alt='' />
 
+        <div className='home_row'>
+
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+
+        <div className='home_row'>
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+
+        </div>
+
+        <div className='home_row'>
+           {/* Product */}
+
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/Product.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Product.css';
+
+function Product() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Product
