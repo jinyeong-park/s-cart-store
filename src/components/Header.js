2d5eba727e7f9935c77ae3f3882ae0a9d26e4228
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-30 12:43:33 -0700

    Add material-ui for icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../styles/Header.css';
 
+
 function Header() {
   return (
     <div className='header'>
 
       <img className='header__logo' src='https://bookable-rooms-images.s3.us-east-2.amazonaws.com/sCart-logo-black.png'/>
       <div className='header__search'>
-        <input className='header__searchInput' type='text' placeholder='Search for anything'>
-        </input>
+        <input className='header__searchInput' type='text' placeholder='Search for anything' />
+
       </div>
 
       <div className='header__nav'>
