d76d7490553bfeb9770caa40dcd866f381b8f06d
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-30 20:11:43 -0700

    Implement link to home when clicking logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,16 @@ import React from 'react';
 import '../styles/Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className='header'>
 
+     {/*  when clicking logo -> go to main homepage */}
+     <Link to='/'>
       <img className='header__logo' src='https://i.imgur.com/Q3TUC8Z_d.webp?maxwidth=728&fidelity=grand'/>
+     </Link>
       <div className='header__search'>
         <input className='header__searchInput' type='text' placeholder='Search..' />
         <SearchIcon className='header__searchIcon'/>
