e35a80159792b78534e37c47c90ed72983523782
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-10-30 23:09:02 -0700

    Suttotal component with design

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import '../styles/Subtotal.css'
+import '../styles/Subtotal.css';
+import CurrencyFormat from 'react-currency-format';
 
 function Subtotal() {
   return (
     <div className='subtotal'>
+      <CurrencyFormat
+        renderText={(value) => (
+            <p>
+              Subtotal (0 items):
+              <strong> 0</strong>
+            </p>
+         )}
+         decimalScale={2}
+         value={0}
+         displayType={"text"}
+         thousandSeparator={true}
+         prefix={"$"}
+      />
 
+      <button>Proceed to Checkout</button>
     </div>
-  )
+  );
 }
 
 export default Subtotal
