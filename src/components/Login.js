d67ec01e17d0949249a7a6219ebed9bf4b841f33
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-11-01 11:13:28 -0800

    Add a Form for Sign-in and Sign-up button

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,6 +10,22 @@ function Login() {
 
         <img className='login__logo' src='https://i.imgur.com/Q3TUC8Z_d.webp?maxwidth=728&fidelity=grand' />
       </Link>
+
+      <div className='login__container'>
+        <h1>Sign-in</h1>
+
+        <form>
+          <h5>E-mail</h5>
+          <input type='text' />
+
+          <h5>Password</h5>
+          <input type='password' />
+
+          <button className='login__signInButton'>Sign In</button>
+        </form>
+
+        <button className='login__signUpButton'>Create your sCart Account</button>
+      </div>
     </div>
   )
 }
