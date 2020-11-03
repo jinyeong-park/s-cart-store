974ae8e8e227a6c5731effacf23f01a4ca9abe59
Jinyeong Park <jinyeongpark1205@gmail.com>
2020-11-03 13:57:48 -0800

    Link item num to checkout in the Payment

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Payment.css';
 import { useStateValue } from '../StateProvider.js';
 import CheckoutItem from './CheckoutItem';
@@ -9,24 +10,29 @@ function Payment() {
   return (
     <div className='payment'>
       <div className='payment__container'>
+        <h1>
+          Checkout {
+            <Link to='/checkout'>{cart?.length} items</Link>
+          }
+        </h1>
+
         {/* payment info for address */}
         <div className='payment__section'>
           <div className='payment_title'>
             <h3>Delivery Address</h3>
           </div>
           <div className='payment__address'>
-            <p>{user?.email}</p>
+            <p>Email: {user?.email}</p>
             <p>770 Harrison St</p>
             <p>Santa Clara, CA 95050</p>
           </div>
-
         </div>
 
         {/* payment info for review items */}
         <div className='payment_section'>
           <div className='payment_title'>
             <h3>Review items and delivery</h3>
-        </div>
+          </div>
         <div className='payment_items'>
           {/* reuse the CheckoutItem component to show checkoout item */}
           {cart.map(item =>
@@ -39,14 +45,14 @@ function Payment() {
               rating= {item.rating}
             />)}
         </div>
-        </div>
+      </div>
 
         {/* payment infor for payment method */}
         <div className='payment_section'>
           <div className='payment__title'>
             <h3>Payment Method</h3>
           </div>
-          <div className='payment__title'>
+          <div className='payment__details'>
 
 
           </div>
